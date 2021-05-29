7c9b655c59b51f2337f24bc3f59a65a2a6728d18
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-29 12:13:39 -0500

    more tests on greeting comp

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 test("renders 'Hello World' as a text", () => {
@@ -13,10 +14,34 @@ test("renders 'Hello World' as a text", () => {
   expect(helloWorldEl).toBeInTheDocument();
 });
 
-test("renders 'Great to be seeing you today...' as text", () => {
+test("renders 'Great to be seeing you today...' if button was NOT clicked", () => {
   render(<Greeting />);
   const greetingEl = screen.getByText("Great to be seeing you today...");
   expect(greetingEl).toBeInTheDocument();
 });
 
-// test();
+test("renders 'Changed!' if the button WAS clicked", () => {
+  //Arrange
+  render(<Greeting />);
+
+  //Act
+  const buttonEl = screen.getByRole("button");
+  userEvent.click(buttonEl);
+
+  //Assert
+  const changedEl = screen.getByText("Changed!");
+  expect(changedEl).toBeInTheDocument();
+});
+
+test("does not render 'good to see you' if button was clicked", () => {
+  //Arrange
+  render(<Greeting />);
+
+  //Act
+  const buttonEl = screen.getByRole("button");
+  userEvent.click(buttonEl);
+
+  //Assert
+  const changedEl = screen.queryByText("good to see you", { exact: false });
+  expect(changedEl).toBeNull();
+});
