53c8b8d0e89b2e90dc053529596deb7b9cbdce9d
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-29 12:29:18 -0500

    mock fetch api for json return in async comp

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,11 @@ import Async from "./Async";
 
 describe("Aysnc Component", () => {
   test("renders posts", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First Post" }],
+    });
+
     render(<Async />);
 
     const listItemEls = await screen.findAllByRole("listitem");
