cb54d774396e28b7f6549c62f0e410b44e5ec981
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-29 11:58:08 -0500

    first test written on greeting comp

---
+++ src/components/Greeting.js
@@ -0,0 +1,11 @@
+const Greeting = () => {
+  return (
+    <div>
+      {" "}
+      <h2>Hello World!!</h2>
+      <p>Great to be seeing you today...</p>
+    </div>
+  );
+};
+
+export default Greeting;
